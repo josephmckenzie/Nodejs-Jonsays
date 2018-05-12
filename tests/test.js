6dedd41845b32613929f2986b29c00f19bead8e2
Joseph Mckenzie <josephmckenzieaz@yahoo.com>
2018-05-12 17:56:19 -0400

    added more tests, updates to sayings (phrases) page

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,7 @@
 
 var test = require('unit.js');
 var index = require('../index.js');
+var sayings = require('../sayings.js')
 
 describe('Tests index', function() {
   it('verifies successful response', function(done) {
@@ -16,4 +17,55 @@ describe('Tests index', function() {
       }
     });
   });
+
+  it('Has Correct Nav Links', function(done) {
+    index.get({ /* event */ }, { /* context */ }, (err, result) => {
+      // console.log(result.body)
+      try {
+        test.number(result.statusCode).is(200);
+        test.value(result.body).contains('Home');
+        test.string(result.body).contains('Mined Minds');
+        test.string(result.body).contains('Jon Says Phrases');
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+
+
+
+});
+
+describe('Tests Sayings', function() {
+  it('verifies successful response', function(done) {
+    sayings.get({ /* event */ }, { /* context */ }, (err, result) => {
+      try {
+        test.number(result.statusCode).is(200);
+        test.string(result.body).contains('Add your own Jon Says Phrase today');
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+
+  it('Has Correct Nav Links', function(done) {
+    index.get({ /* event */ }, { /* context */ }, (err, result) => {
+      // console.log(result.body)
+      try {
+        test.number(result.statusCode).is(200);
+        test.value(result.body).contains('Home');
+        test.string(result.body).contains('Mined Minds');
+        test.string(result.body).contains('Jon Says Phrases');
+        test.value(result).hasHeader('content-type', 'text/html');
+        done();
+      } catch(error) {
+        done(error);
+      }
+    });
+  });
+
 });
